f47d55baa9d705819b9e4d3b96a3a6ad58d5cf95
Nathan Martinigol <nmartinigol@iutbayonne.univ-pau.fr>
2020-11-27 15:26:25 +0100

    Mise en place du controller métier

---
+++ src/Controller/ProStageControllrController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProStageControllrController extends AbstractController
+{
+    /**
+     * @Route("/pro/stage/controllr", name="pro_stage_controllr")
+     */
+    public function index(): Response
+    {
+        return $this->render('pro_stage_controllr/index.html.twig', [
+            'controller_name' => 'ProStageControllrController',
+        ]);
+    }
+}
